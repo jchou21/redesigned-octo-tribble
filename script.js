a464c9fd7edfd72c2f4fc46364e68510964491c3
Jared <jaredhappy@gmail.com>
2019-05-30 19:55:00 -0700

    started adding image animation

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 $(document).ready(function(){
-$("#dropdown-content").hide();
-var body = true;
-$(document).dblclick(function(e) {
-    if(e.target.classList.contains('content')) {
-        $('.content').fadeOut('slow');
-        body = false;
-    } else if(e.target.getAttribute("id") == "baited") {
-        var baited = $(".content").append("<p>lol baited</p>");
-    }
-})
-$("#header").mouseenter(function(){
-    if(!body){
-        $(".content").fadeIn("slow");
-        body = true;
-    }
-})
-$(".dropdown").hover(function(){
-    $("#dropdown-content").show();
-    // $("#header > h2").css("backgroundColor","tomato");
-    $("#dropdown-content").css("width", $("#header > h2").outerWidth());
-    $("#header > h2").mouseout(function(){
-        $(this).css("background-color" , "tomato");
-    })
-    $("#header > h2").mouseover(function(){
-        $(this).css("background-color","aqua");
-    })
-},function(){
     $("#dropdown-content").hide();
-    $("#header > h2").css("background-color","transparent");
-})
+    var body = true;
+    var firstTime = true;
+    $(document).dblclick(function(e) {
+        if(e.target.classList.contains('content')) {
+            $('.content').fadeOut('slow');
+            body = false;
+        } else if(e.target.getAttribute("id") == "baited") {
+            var baited = $(".content").append("<p>lol baited</p>");
+        }
+    })
+    $("#header").mouseenter(function(){
+        if(!body){
+            $(".content").fadeIn("slow");
+            body = true;
+        }
+    })
+    $(".dropdown").hover(function(){
+        if(firstTime){
+            alert("The websites under other websites go decending in quality in my opinion");
+            firstTime = false;
+        }
+        $("#dropdown-content").show();
+        $("#dropdown-content").css("width", $("#header > h2").outerWidth());
+        $("#header > h2").mouseout(function(){
+            $(this).css("background-color" , "tomato");
+        })
+        $("#header > h2").mouseover(function(){
+            $(this).css("background-color","aqua");
+        })
+    },function(){
+        $("#dropdown-content").hide();
+        $("#header > h2").css("background-color","transparent");
+    })
+    $("img").mouseenter(function(){
+        $(this).animate({
+            height:'50%',
+            width:'50%',
+            opacity: '0.75'
+        },'slow');
+        $(this).animate({
+            height:'100%',
+            width: '100%',
+            opacity: '1'
+        },'slow',function(){
+            $(this).removeAttr('style');
+        });
+    })
 });
